4c223c7fb19d10b406af2d8c873681999ce13d7a
耗子 <i@haozi.net>
2024-12-16 19:50:05 +0800

    fix: lint

--- client.go
+++ client.go
@@ -218,7 +218,7 @@ func (c *Client) validateTicket(ticket string, service *http.Request) error {
 		return err
 	}
 
-	if err := c.tickets.Write(ticket, success); err != nil {
+	if err = c.tickets.Write(ticket, success); err != nil {
 		return err
 	}
 
